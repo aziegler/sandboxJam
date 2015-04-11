f1e969a3366d4619f2507b34e37f5201aa25a225
Damien <baal.chouchen@gmail.com>
2015-04-11 16:16:39 +0200

    generate flower.

--- Assets/scripts/Planet.cs
+++ Assets/scripts/Planet.cs
@@ -4,10 +4,31 @@ using System.Collections;
 public class Planet : MonoBehaviour {
 
     public static Planet Instance;
+    CircleCollider2D circle;
+    public GameObject PrefabFlower;
+    public float FlowerSpawnAngle = 10f;
 
     void Awake ()
     {
         Instance = this;
+        circle = GetComponent<CircleCollider2D>();
+    }
+
+    void Start ()
+    {
+        float total = 0f;
+
+        do
+        {
+            GameObject go = GameObject.Instantiate(PrefabFlower);
+            go.transform.position = Vector3.zero;
+            go.transform.eulerAngles = new Vector3(0f, 0f, total);
+            go.transform.Translate(Vector3.up * (circle.radius));
+            go.transform.parent = transform;
+
+            total += FlowerSpawnAngle;
+
+        } while (total < 360f);
     }
 
     public float GetOrientedAngle (Transform t)
