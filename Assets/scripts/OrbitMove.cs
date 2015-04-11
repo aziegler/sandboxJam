a98569732a4f9ffff15d6e38f93aebe8b306c44d
Damien <baal.chouchen@gmail.com>
2015-04-11 11:19:07 +0200

    Add orbrit scripts.

---
+++ Assets/scripts/OrbitMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbitMove : MonoBehaviour {
+
+    Transform planet;
+    Rigidbody2D rb;
+    public float Force = .1f;
+
+	// Use this for initialization
+	void Start () 
+    {      
+        planet = GameObject.FindGameObjectWithTag("Planet").transform;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector2 direction = planet.position - transform.position;
+        direction.Normalize();
+
+        rb.velocity += (direction*Force);
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //
+    }
+}
