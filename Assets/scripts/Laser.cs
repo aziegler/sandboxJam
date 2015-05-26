def09bbbf169ce886a264ab25b56f5513a5f3a25
Damien Blanchard <metalchouchen@gmail.com>
2015-05-26 23:29:12 +0200

    Add fade on particle

--- Assets/scripts/Laser.cs
+++ Assets/scripts/Laser.cs
@@ -98,7 +98,7 @@ public class Laser : MonoBehaviour
 
     private void DestroyMud()
     {
-        Destroy(_explosion.gameObject);
+        Destroy(_explosion.gameObject,5F);
         _explosion = null;
     }
 
